dd95cba71aebaefb698f154c387dd7616eafa809
minbo2002 <minbo2002@naver.com>
2024-06-16 21:39:38 +0900

    [feat] chap1 토비스프링 환경설정

---
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -0,0 +1,33 @@
+package com.example.tobyspring;
+
+import com.example.tobyspring.user.User;
+import com.example.tobyspring.user.dao.UserDao;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class TobySpringApplication {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        SpringApplication.run(TobySpringApplication.class, args);
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("minbo");
+        user.setName("김민보");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+
+}
---
+++ src/main/java/com/example/tobyspring/user/User.java
@@ -0,0 +1,12 @@
+package com.example.tobyspring.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String id;
+    private String name;
+    private String password;
+}
